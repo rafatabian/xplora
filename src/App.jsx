d33c6b82f25433097dffb89e47292b0b9a4a9372
Rata Fabian <rata.fabian1@gmail.com>
2023-10-07 17:30:11 +0100

    solving issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { HashRouter as Router, Route, Routes } from "react-router-dom"
 import Home from "./Containers/Home/Home"
 import Navbar from "./Containers/Navbar/Navbar"
 
